343bce7371522315155ee13937d8dd25bf36a8ff
Slava Zakharov <90175094+petyapetr@users.noreply.github.com>
2025-10-31 16:22:56 +0400

    Add eta

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
-import {readFileSync} from "node:fs";
 import {readFile, stat} from "node:fs/promises";
 import {join} from "node:path";
 import {H3, serve, html, serveStatic} from "h3";
-import Mustache from "mustache";
+import {Eta} from "eta";
 
 const app = new H3();
+
+const eta = new Eta({
+	views: join(import.meta.dirname, "templates"),
+});
+
 app.use("/public/**", (event) =>
 	serveStatic(event, {
 		getContents: (id) => {
@@ -22,9 +26,19 @@ app.use("/public/**", (event) =>
 	})
 );
 app.get("/", (e) => "⚡️ Tadaa!");
+// app.get("/about", (e) => {
+// 	const template = readFileSync("src/templates/about.html", "utf-8");
+// 	const content = Mustache.render(template, {text: "about"});
+
+// 	return html(content);
+// });
 app.get("/about", (e) => {
-	const template = readFileSync("src/templates/about.html", "utf-8");
-	const content = Mustache.render(template, {text: "about"});
+	const content = eta.render("pages/about.html", {text: "About eta"});
+
+	return html(content);
+});
+app.get("/foo", (e) => {
+	const content = eta.render("pages/foo", {text: "Foo"});
 
 	return html(content);
 });
