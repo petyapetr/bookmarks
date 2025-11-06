b991162b5e803f12a3d253baa9b3764e0f20fe94
Slava Zakharov <90175094+petyapetr@users.noreply.github.com>
2025-11-06 17:06:40 +0400

    Add db
    1. Add schema
    2. Add bookmarks table
    3. Add card table
    4. Add page header

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS bookmarks (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  url TEXT NOT NULL,
+  name TEXT,
+  note TEXT,
+  read_in_year INTEGER,
+  read_in_month INTEGER,
+  created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+  updated_at TEXT DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS bookmark_tags (
+  bookmark_id INTEGER NOT NULL,
+  tag_id INTEGER NOT NULL,
+  PRIMARY KEY (bookmark_id, tag_id),
+  FOREIGN KEY (bookmark_id) REFERENCES bookmarks(id),
+  FOREIGN KEY (tag_id) REFERENCES tags(id)
+);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import {readFile, stat} from "node:fs/promises";
 import {join} from "node:path";
-import {H3, serve, serveStatic} from "h3";
+import {H3, serve, serveStatic, redirect} from "h3";
 import {render} from "./renderer.js";
+import {
+	getOverallCount,
+	getUnreadCount,
+	getBookmarks,
+	getBookmark,
+	createBookmark,
+} from "./db.js";
 
 const app = new H3();
 
@@ -21,10 +28,57 @@ app.use("/public/**", (event) =>
 		},
 	})
 );
-app.get("/", (e) => "⚡️ Tadaa!");
-app.get("/about", (e) =>
-	render("pages/about.html", {text: "About njk templates"})
-);
-app.get("/foo", (e) => render("pages/foo.html", {user: {name: "hal"}}));
+app.use("**", (e) => {
+	const all = getOverallCount();
+	const unread = getUnreadCount();
+	e.context.count = {all, unread};
+});
+app.get("/", (e) => {
+	return render("pages/home.html", {count: e.context.count});
+});
+app.get("/search", (e) => {
+	const currentYear = new Date().getFullYear();
+	const years = [];
+	for (let year = 2024; year <= currentYear; year++) {
+		years.push(year);
+	}
+
+	return render("pages/search.html", {
+		years,
+		tags: ["code", "star", "work", "best", "gis", "blog"],
+		count: e.context.count,
+	});
+});
+app.get("/bookmarks", (e) => {
+	const bookmarks = getBookmarks();
+	console.log(bookmarks[0]);
+
+	return render("pages/bookmarks.html", {
+		count: e.context.count,
+		bookmarks,
+		query: 'None'
+	});
+});
+app.post("/bookmarks", async (e) => {
+	const body = await e.req.formData();
+	const url = body.get("url");
+	// TODO: add error code
+	if (url === null) throw new Error("Wrong input");
+	const name = body.get("name") ?? new URL(url).hostname;
+	const note = body.get("note");
+	const tags = body.get("tags")?.split(",");
+	const res = createBookmark(url, name, note, tags);
+	return redirect("/bookmarks/" + res);
+});
+app.get("/bookmarks/:id", (e) => {
+	const id = e.context.params.id;
+	// TODO: add error code
+	if (typeof (id * 1) !== "number") throw new Error("Not Allowed");
+	const data = getBookmark(id);
+	return render("pages/card.html", {
+		count: e.context.count,
+		...data,
+	});
+});
 
 serve(app, {port: process.env.PORT || 3000});
